d05abd06b2620334be251a0cd6cdbe3405191e52
wangkc <wkclz@qq.com>
2019-11-01 23:56:11 +0800

    Constant

--- src/main/java/com/wkclz/demo/config/PasswordEncrypt.java
+++ src/main/java/com/wkclz/demo/config/PasswordEncrypt.java
@@ -1,12 +1,12 @@
 package com.wkclz.demo.config;
 
-import com.wkclz.core.base.Constant;
+import com.wkclz.core.constant.BaseConstant;
 import com.wkclz.core.util.SecretUtil;
 
 public class PasswordEncrypt {
 
     public static void main(String[] args) {
         String password = SecretUtil.getEncryptPassword("password");
-        System.out.println("处理后的密码是：" +Constant.CONFIG_ENCRYPTED_PREFIX + password);
+        System.out.println("处理后的密码是：" + BaseConstant.CONFIG_ENCRYPTED_PREFIX + password);
     }
 }
