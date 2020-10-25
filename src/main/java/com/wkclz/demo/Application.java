ed106ced8ed9c78af959dcb68840749d87fa1b2f
wkclz <wkclz@qq.com>
2020-10-25 10:31:08 +0800

    解密扫描

--- src/main/java/com/wkclz/demo/Application.java
+++ src/main/java/com/wkclz/demo/Application.java
@@ -1,11 +1,13 @@
 package com.wkclz.demo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import com.wkclz.core.base.Sys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
 
     public static void main(String[] args) {
